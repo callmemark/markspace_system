17cf04d302bcc02ed15d05c02d75bc2775a59439
callmemark <zzzmrmeowzzz@gmail.com>
2026-06-03 20:25:05 +0800

    Updated server host

--- markspace_system/settings.py
+++ markspace_system/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = True
-ALLOWED_HOSTS = ["13.212.1.135", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["13.212.77.186", "127.0.0.1", "localhost"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
