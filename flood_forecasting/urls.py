f888fb016f72858a8fd30e43a9f9ef10dfe9de54
callmemark <zzzmrmeowzzz@gmail.com>
2026-06-06 20:18:02 +0800

    Fix bug

--- flood_forecasting/urls.py
+++ flood_forecasting/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import FloodDepthView, FloodDepthGeoJSONView
+from .views import FloodDepthView
 
 urlpatterns = [
     path('api/flood-depth/', FloodDepthView.as_view(), name='flood-depth'),
