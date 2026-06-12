2cab90a5759ddeadccaa3e287d4ea00f94b94229
callmemark <zzzmrmeowzzz@gmail.com>
2026-06-12 14:59:00 +0800

    Updated login page

--- iam_system/utils.py
+++ iam_system/utils.py
@@ -5,15 +5,18 @@ from django.conf import settings
 
 signer = TimestampSigner()
 
+
 def generate_verification_token(account):
     return signer.sign(str(account.id))
 
-def verify_token(token, max_age=86400):  # 24 hours
+
+def verify_token(token, max_age=86400): 
     try:
         account_id = signer.unsign(token, max_age=max_age)
         return account_id
     except (BadSignature, SignatureExpired):
         return None
+
 
 def send_verification_email(account):
     token = generate_verification_token(account)
--- markspace_system/urls.py
+++ markspace_system/urls.py
@@ -2,7 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include  
 
-
+admin.site.site_header = "Admin Panel"
+admin.site.site_title = "Admin Panel"
+admin.site.index_title = "Welcom Back Mark!"
 
 urlpatterns = [
     path('', include('homepage.urls')),
